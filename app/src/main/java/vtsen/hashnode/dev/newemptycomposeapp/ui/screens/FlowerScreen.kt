ffb5b50aad5bda98f8f57b7ae8995c0cc52b5cdb
Morteza-Nazifi <mnazifee@gmail.com>
2026-07-09 13:15:54 +0330

    Create FlowerScreen.kt

---
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/screens/FlowerScreen.kt
@@ -0,0 +1,30 @@
+package vtsen.hashnode.dev.newemptycomposeapp.ui.screens
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FlowerScreen() {
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            // در مرحله بعد گل هشت‌گلبرگی اینجا رسم خواهد شد.
+
+        }
+
+    }
+
+}
