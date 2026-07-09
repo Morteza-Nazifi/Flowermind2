4d4d46ab612dc5deae92eb5faca7dd927d26b4cb
Morteza-Nazifi <mnazifee@gmail.com>
2026-07-09 12:20:52 +0330

    Update PetalData.kt

--- app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/model/PetalData.kt
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/model/PetalData.kt
@@ -1,1 +1,13 @@
 
+package vtsen.hashnode.dev.newemptycomposeapp.ui.model
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class PetalData(
+    val id: Int,
+    val title: String,
+    val icon: ImageVector,
+    val color: Color,
+    val angle: Float
+)
