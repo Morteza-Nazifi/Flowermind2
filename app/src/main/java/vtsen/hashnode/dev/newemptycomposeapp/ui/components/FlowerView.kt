670d666940101408bf523f01c1d7cfa78332766d
Morteza-Nazifi <mnazifee@gmail.com>
2026-07-09 19:15:37 +0330

    Update FlowerView.kt

--- app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/components/FlowerView.kt
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/components/FlowerView.kt
@@ -2,9 +2,15 @@ package vtsen.hashnode.dev.newemptycomposeapp.ui.components
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
+import kotlin.math.cos
+import kotlin.math.sin
 
 @Composable
 fun FlowerView() {
@@ -13,6 +19,37 @@ fun FlowerView() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        FlowerCanvas()
+
+        val radius = 135f
+
+        petals.forEachIndexed { index, petal ->
+
+            val indexionZicsLayert = Math.toRadians((index * 45.0) - 90.0)
+
+            val x = (cos(angle) * radius).dp
+
+            val y = (sin(angle) * radius).dp
+
+            Box(
+                modifier = Modifier
+                    .offset(x = x, y = y)
+                    .graphicsLayer {
+                        rotationZ = index * 45f
+                    }
+            ) {
+
+                petal(
+                    petal = petal,
+                    modifier = Modifier.size(
+                        width = 96.dp,
+                        height = 170.dp
+                    )
+                )
+
+            }
+
+        }
+
     }
+
 }
