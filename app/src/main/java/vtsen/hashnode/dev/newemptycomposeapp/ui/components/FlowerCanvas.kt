1a8921eceada905c49348632dcab3cdeb1c355cf
Morteza-Nazifi <mnazifee@gmail.com>
2026-07-10 10:11:22 +0330

    Update FlowerCanvas.kt

--- app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/components/FlowerCanvas.kt
+++ app/src/main/java/vtsen/hashnode/dev/newemptycomposeapp/ui/components/FlowerCanvas.kt
@@ -7,15 +7,21 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
+import kotlin.math.hypot
 
 @Composable
-fun FlowerCanvas() {
+fun FlowerCanvas(
+    selectedPetal: Int?,
+    onCenterClick: () -> Unit
+) {
 
     val transition = rememberInfiniteTransition(label = "")
 
@@ -34,7 +40,29 @@ fun FlowerCanvas() {
     )
 
     Canvas(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+
+                detectTapGestures { offset ->
+
+                    val center = Offset(
+                        size.width / 2f,
+                        size.height / 2f
+                    )
+
+                    val radius = 42.dp.toPx()
+
+                    if (
+                        hypot(
+                            offset.x - center.x,
+                            offset.y - center.y
+                        ) <= radius
+                    ) {
+                        onCenterClick()
+                    }
+                }
+            }
     ) {
 
         val center = Offset(
@@ -71,6 +99,15 @@ fun FlowerCanvas() {
                 radius = radius,
                 center = center
             )
+        }
+
+        // --------
+        // رزرو برای موج نور گلبرگ
+        // --------
+        if (selectedPetal != null) {
+
+            // در مرحله بعد مسیر نور از مرکز
+            // تا گلبرگ انتخاب شده اینجا رسم خواهد شد.
         }
 
         // هسته نورانی
